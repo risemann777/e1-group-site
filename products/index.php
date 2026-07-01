9a3d7d14231e57b468f2f4a8ff8da4c10d7a458f
Semen Skameykin <risemann777@gmail.com>
2026-07-01 23:47:05 +0300

    include component in products section

--- products/index.php
+++ products/index.php
@@ -5,109 +5,171 @@ $APPLICATION->SetTitle("Продукция и услуги");
 $APPLICATION->SetDirProperty("headline_text", "АО «Е1 Групп» объединяет производство, дистрибуцию и сервис профессионального электроинструмента и строительного оборудования. Ключевые направления: ЭНГЕЛЬС, DongCheng и Distribution for Construction.");
 ?>
 
-  <div class="direction direction--page">
-    <div class="direction__container">
-      <div class="direction__stat">
-        <div class="stat">
-          <div class="stat__list">
-            <div class="stat__item">
-              <div class="stat__label">Экспертиза команды на рынке электроинструмента</div>
-              <div class="stat__info">
-                <div class="stat__value">>30</div>
-                <div class="stat__term">лет</div>
-              </div>
-            </div>
-            <div class="stat__item">
-              <div class="stat__label">Производственная мощность завода ЭНГЕЛЬС</div>
-              <div class="stat__info">
-                <div class="stat__value">>1.5</div>
-                <div class="stat__term">млн.</div>
-              </div>
-            </div>
-            <div class="stat__item">
-              <div class="stat__label">Продажи электроинструмента DongCheng по всему миру</div>
-              <div class="stat__info">
-                <div class="stat__value">>30</div>
-                <div class="stat__term">млн.</div>
-              </div>
-            </div>
-            <div class="stat__item">
-              <div class="stat__label">Брендов для профессионалов в портфолио</div>
-              <div class="stat__info">
-                <div class="stat__value">>5</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="direction__list">
-        <div class="direction__item">
-          <div class="direction__info">
-            <div class="direction__content">
-              <div class="direction__logo">
-                <img src="/upload/main/images/brands/brand_engels.png">
-              </div>
-              <div class="direction__description">Российский бренд профессионального электроинструмента от завода-
-                производителя в г. Энгельс. Производство, разработка продукта, сервис, запчасти и гарантия.
-              </div>
-            </div>
-            <div class="direction__button"><a class="btn btn--outline-primary btn--sm" href="https://engelstool.ru" target="_blank">Подробнее</a>
-            </div>
-          </div>
-          <div class="direction__picture">
-            <div class="direction__image"><a class="direction__clickable" href="https://engelstool.ru" target="_blank" aria-label="Перейти на сайт Энгельс"></a>
-              <picture>
-                <img src="/upload/main/images/directions/direction_1.jpg">
-              </picture>
-            </div>
-          </div>
-        </div>
-        <div class="direction__item">
-          <div class="direction__info">
-            <div class="direction__content">
-              <div class="direction__logo"><img src="/upload/main/images/brands/brand_dong_cheng.png"></div>
-              <div class="direction__description">Профессиональный электроинструмент DongCheng. Широкий ассортимент,
-                надежный сервис и гарантийная поддержка на официальном сайте бренда в России.
-              </div>
-            </div>
-            <div class="direction__button">
-              <a class="btn btn--outline-primary btn--sm" href="https://dongchengtool.ru" target="_blank">Подробнее</a>
-            </div>
-          </div>
-          <div class="direction__picture">
-            <div class="direction__image">
-              <a class="direction__clickable" href="https://dongchengtool.ru" target="_blank" aria-label="Перейти на сайт DongCheng"></a>
-              <picture>
-                <img src="/upload/main/images/directions/direction_2.jpg">
-              </picture>
-            </div>
-          </div>
-        </div>
-        <div class="direction__item">
-          <div class="direction__info">
-            <div class="direction__content">
-              <div class="direction__logo">
-                <img src="/upload/main/images/brands/brand_dfc.png">
-              </div>
-              <div class="direction__description">Поставщик строительного оборудования, электроинструмента,
-                садово-парковой техники и сервисных услуг для профессионального рынка.
-              </div>
-            </div>
-            <div class="direction__button">
-              <a class="btn btn--outline-primary btn--sm" href="https://dctool.ru" target="_blank">Подробнее</a>
-            </div>
-          </div>
-          <div class="direction__picture">
-            <div class="direction__image">
-              <a class="direction__clickable" href="https://dctool.ru" target="_blank" aria-label="Перейти на сайт Distribution For Construction"></a>
-              <picture>
-                <img src="/upload/main/images/directions/direction_3.jpg">
-              </picture>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<?$APPLICATION->IncludeComponent(
+  "bitrix:catalog",
+  "products",
+  [
+    "ACTION_VARIABLE" => "action",
+    "ADD_ELEMENT_CHAIN" => "N",
+    "ADD_PICT_PROP" => "-",
+    "ADD_PROPERTIES_TO_BASKET" => "Y",
+    "ADD_SECTIONS_CHAIN" => "Y",
+    "AJAX_MODE" => "N",
+    "AJAX_OPTION_ADDITIONAL" => "",
+    "AJAX_OPTION_HISTORY" => "N",
+    "AJAX_OPTION_JUMP" => "N",
+    "AJAX_OPTION_STYLE" => "Y",
+    "BASKET_URL" => "/personal/basket.php",
+    "CACHE_FILTER" => "N",
+    "CACHE_GROUPS" => "Y",
+    "CACHE_TIME" => "36000000",
+    "CACHE_TYPE" => "A",
+    "COMPATIBLE_MODE" => "N",
+    "DETAIL_ADD_DETAIL_TO_SLIDER" => "N",
+    "DETAIL_BACKGROUND_IMAGE" => "-",
+    "DETAIL_BRAND_USE" => "N",
+    "DETAIL_BROWSER_TITLE" => "-",
+    "DETAIL_CHECK_SECTION_ID_VARIABLE" => "N",
+    "DETAIL_DETAIL_PICTURE_MODE" => [
+      0 => "POPUP",
+      1 => "MAGNIFIER",
+    ],
+    "DETAIL_DISPLAY_NAME" => "Y",
+    "DETAIL_DISPLAY_PREVIEW_TEXT_MODE" => "E",
+    "DETAIL_IMAGE_RESOLUTION" => "16by9",
+    "DETAIL_META_DESCRIPTION" => "-",
+    "DETAIL_META_KEYWORDS" => "-",
+    "DETAIL_PRODUCT_INFO_BLOCK_ORDER" => "sku,props",
+    "DETAIL_PRODUCT_PAY_BLOCK_ORDER" => "rating,price,priceRanges,quantityLimit,quantity,buttons",
+    "DETAIL_SET_CANONICAL_URL" => "N",
+    "DETAIL_SHOW_POPULAR" => "Y",
+    "DETAIL_SHOW_SLIDER" => "N",
+    "DETAIL_SHOW_VIEWED" => "Y",
+    "DETAIL_STRICT_SECTION_CHECK" => "N",
+    "DETAIL_USE_COMMENTS" => "N",
+    "DETAIL_USE_VOTE_RATING" => "N",
+    "DISABLE_INIT_JS_IN_COMPONENT" => "N",
+    "DISPLAY_BOTTOM_PAGER" => "Y",
+    "DISPLAY_TOP_PAGER" => "N",
+    "ELEMENT_SORT_FIELD" => "sort",
+    "ELEMENT_SORT_FIELD2" => "id",
+    "ELEMENT_SORT_ORDER" => "asc",
+    "ELEMENT_SORT_ORDER2" => "desc",
+    "FILTER_HIDE_ON_MOBILE" => "N",
+    "FILTER_VIEW_MODE" => "VERTICAL",
+    "IBLOCK_ID" => "2",
+    "IBLOCK_TYPE" => "products",
+    "INCLUDE_SUBSECTIONS" => "Y",
+    "INSTANT_RELOAD" => "N",
+    "LABEL_PROP" => [
+    ],
+    "LAZY_LOAD" => "N",
+    "LINE_ELEMENT_COUNT" => "3",
+    "LINK_ELEMENTS_URL" => "link.php?PARENT_ELEMENT_ID=#ELEMENT_ID#",
+    "LINK_IBLOCK_ID" => "",
+    "LINK_IBLOCK_TYPE" => "",
+    "LINK_PROPERTY_SID" => "",
+    "LIST_BROWSER_TITLE" => "-",
+    "LIST_ENLARGE_PRODUCT" => "STRICT",
+    "LIST_META_DESCRIPTION" => "-",
+    "LIST_META_KEYWORDS" => "-",
+    "LIST_PRODUCT_BLOCKS_ORDER" => "price,props,sku,quantityLimit,quantity,buttons",
+    "LIST_PRODUCT_ROW_VARIANTS" => "[{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false}]",
+    "LIST_SHOW_SLIDER" => "Y",
+    "LIST_SLIDER_INTERVAL" => "3000",
+    "LIST_SLIDER_PROGRESS" => "N",
+    "LOAD_ON_SCROLL" => "N",
+    "MESSAGE_404" => "",
+    "MESS_BTN_ADD_TO_BASKET" => "В корзину",
+    "MESS_BTN_BUY" => "Купить",
+    "MESS_BTN_COMPARE" => "Сравнение",
+    "MESS_BTN_DETAIL" => "Подробнее",
+    "MESS_BTN_LAZY_LOAD" => "Показать ещё",
+    "MESS_BTN_SUBSCRIBE" => "Подписаться",
+    "MESS_NOT_AVAILABLE" => "Нет в наличии",
+    "MESS_NOT_AVAILABLE_SERVICE" => "Недоступно",
+    "PAGER_BASE_LINK_ENABLE" => "N",
+    "PAGER_DESC_NUMBERING" => "N",
+    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+    "PAGER_SHOW_ALL" => "N",
+    "PAGER_SHOW_ALWAYS" => "N",
+    "PAGER_TEMPLATE" => ".default",
+    "PAGER_TITLE" => "Товары",
+    "PAGE_ELEMENT_COUNT" => "30",
+    "PARTIAL_PRODUCT_PROPERTIES" => "N",
+    "PRICE_CODE" => [
+    ],
+    "PRICE_VAT_INCLUDE" => "Y",
+    "PRICE_VAT_SHOW_VALUE" => "N",
+    "PRODUCT_ID_VARIABLE" => "id",
+    "PRODUCT_PROPS_VARIABLE" => "prop",
+    "PRODUCT_QUANTITY_VARIABLE" => "quantity",
+    "SEARCH_CHECK_DATES" => "Y",
+    "SEARCH_NO_WORD_LOGIC" => "Y",
+    "SEARCH_PAGE_RESULT_COUNT" => "50",
+    "SEARCH_RESTART" => "N",
+    "SEARCH_USE_LANGUAGE_GUESS" => "Y",
+    "SEARCH_USE_SEARCH_RESULT_ORDER" => "N",
+    "SECTIONS_SHOW_PARENT_NAME" => "Y",
+    "SECTIONS_VIEW_MODE" => "LIST",
+    "SECTION_BACKGROUND_IMAGE" => "-",
+    "SECTION_COUNT_ELEMENTS" => "Y",
+    "SECTION_ID_VARIABLE" => "SECTION_ID",
+    "SECTION_TOP_DEPTH" => "2",
+    "SEF_MODE" => "Y",
+    "SET_LAST_MODIFIED" => "N",
+    "SET_STATUS_404" => "N",
+    "SET_TITLE" => "Y",
+    "SHOW_404" => "N",
+    "SHOW_DEACTIVATED" => "N",
+    "SHOW_PRICE_COUNT" => "1",
+    "SHOW_SKU_DESCRIPTION" => "N",
+    "SHOW_TOP_ELEMENTS" => "Y",
+    "SIDEBAR_DETAIL_SHOW" => "N",
+    "SIDEBAR_PATH" => "",
+    "SIDEBAR_SECTION_SHOW" => "Y",
+    "TEMPLATE_THEME" => "blue",
+    "TOP_ELEMENT_COUNT" => "9",
+    "TOP_ELEMENT_SORT_FIELD" => "sort",
+    "TOP_ELEMENT_SORT_FIELD2" => "id",
+    "TOP_ELEMENT_SORT_ORDER" => "asc",
+    "TOP_ELEMENT_SORT_ORDER2" => "desc",
+    "TOP_ENLARGE_PRODUCT" => "STRICT",
+    "TOP_LINE_ELEMENT_COUNT" => "3",
+    "TOP_PRODUCT_BLOCKS_ORDER" => "price,props,sku,quantityLimit,quantity,buttons",
+    "TOP_PRODUCT_ROW_VARIANTS" => "[{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false}]",
+    "TOP_SHOW_SLIDER" => "Y",
+    "TOP_SLIDER_INTERVAL" => "3000",
+    "TOP_SLIDER_PROGRESS" => "N",
+    "TOP_VIEW_MODE" => "SECTION",
+    "USER_CONSENT" => "N",
+    "USER_CONSENT_ID" => "0",
+    "USER_CONSENT_IS_CHECKED" => "Y",
+    "USER_CONSENT_IS_LOADED" => "N",
+    "USE_COMPARE" => "N",
+    "USE_ELEMENT_COUNTER" => "Y",
+    "USE_ENHANCED_ECOMMERCE" => "N",
+    "USE_FILTER" => "N",
+    "USE_MAIN_ELEMENT_SECTION" => "N",
+    "USE_PRICE_COUNT" => "N",
+    "USE_PRODUCT_QUANTITY" => "N",
+    "USE_STORE" => "N",
+    "COMPONENT_TEMPLATE" => "products",
+    "SEF_FOLDER" => "/products/",
+    "SEF_URL_TEMPLATES" => [
+      "sections" => "",
+      "section" => "#SECTION_CODE#/",
+      "element" => "#SECTION_CODE#/#ELEMENT_ID#/",
+      "compare" => "compare.php?action=#ACTION_CODE#",
+      "smart_filter" => "#SECTION_ID#/filter/#SMART_FILTER_PATH#/apply/",
+    ],
+    "VARIABLE_ALIASES" => [
+      "compare" => [
+        "ACTION_CODE" => "action",
+      ],
+    ]
+  ],
+  false
+);?>
 
 <?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
